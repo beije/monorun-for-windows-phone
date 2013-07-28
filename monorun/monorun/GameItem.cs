0d8f1f5d7a608d7c9a7381157925d00c48e73450
beije <git@benjaminhorn.se>
2013-07-28 14:33:43 +0200

    Made some performance optimizations on the collision detection.

--- monorun/monorun/GameItem.cs
+++ monorun/monorun/GameItem.cs
@@ -12,6 +12,7 @@ namespace monorun
         // Animation representing the player
         public Texture2D ItemTexture;
         private Color[] textureData;
+       
 
         // Position of the Player relative to the upper left side of the screen
         public Vector2 Position;
@@ -25,7 +26,7 @@ namespace monorun
         public virtual void Initialize(Texture2D texture, Vector2 position)
         {
             ItemTexture = texture;
-
+            textureData = generateTextureData();
             // Set the starting position of the player around the middle of the screen and to the back
             Position = position;
         }
@@ -39,11 +40,14 @@ namespace monorun
         {
             spriteBatch.Draw(ItemTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
         }
+        private Color[] generateTextureData()
+        {
+            Color[] td = new Color[ItemTexture.Width * ItemTexture.Height];
+            ItemTexture.GetData(td);
+
+            return td;
+        }
         public virtual Color[] getTextureData() {
-
-            textureData = new Color[ItemTexture.Width * ItemTexture.Height];
-            ItemTexture.GetData(textureData);
-
             return textureData;
         }
         public virtual Rectangle getItemRectangle() 
--- monorun/monorun/GamePage.xaml.cs
+++ monorun/monorun/GamePage.xaml.cs
@@ -94,6 +94,7 @@ namespace monorun
 
         public void addRoland(object sender, GameTimerEventArgs e)
         {
+            if (rolands.Count >= 5) return;
             int ScreenWidth = SharedGraphicsDeviceManager.Current.GraphicsDevice.Viewport.Width;
             int ScreenHeight = SharedGraphicsDeviceManager.Current.GraphicsDevice.Viewport.Height;
             Random rnd = new Random();
@@ -159,13 +160,26 @@ namespace monorun
             }
         }
 
+        static bool intersectsBox( Rectangle source, Rectangle target ) {
+        
+		    return !(
+                ((source.Y + source.Height) < (target.Y)) ||
+			    ( source.Y > ( target.Y + target.Height ) ) ||
+			    ( ( source.X + source.Width ) < target.X ) ||
+                (source.X > (target.X + target.Width)) 
+		    );
+
+        }
+
         static bool IntersectsPixel(Rectangle rect1, Color[] data1, Rectangle rect2, Color[] data2)
         {
+            if (!intersectsBox(rect1, rect2)) return false;
+
             int top = Math.Max( rect1.Top, rect2.Top );
             int bottom = Math.Min(rect1.Bottom, rect2.Bottom);
             int left = Math.Max(rect1.Left, rect2.Left);
             int right = Math.Min(rect1.Right, rect2.Right);
-
+            System.Diagnostics.Debug.WriteLine(top+' '+bottom+" "+left+" "+right);
             for (int y = top; y < bottom; y++)
             {
                 for (int x = left; x < right; x++)
