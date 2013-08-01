e880170c8b4ccdf5a7d55afa678d589e8dc0d93d
beije <git@benjaminhorn.se>
2013-08-01 21:40:56 +0200

    Added EndGamePage

---
+++ monorun/monorun/EndGamePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace monorun
+{
+    public partial class EndGamePage : PhoneApplicationPage
+    {
+		public EndGamePage()
+        {
+            InitializeComponent();
+/*
+			HighscoreList.Items.Add("Phoebe Capricornus");
+			HighscoreList.Items.Add("Phoebe Fornax");
+			HighscoreList.Items.Add("Iapetos Coma Beren");
+			HighscoreList.Items.Add("Rhea Pictor");
+			HighscoreList.Items.Add("Hyperion Taurus");
+			HighscoreList.Items.Add("Kreios Andromeda");
+			HighscoreList.Items.Add("Phoebe Fornax");
+			HighscoreList.Items.Add("Iapetos Coma Beren");
+			HighscoreList.Items.Add("Rhea Pictor");
+			HighscoreList.Items.Add("Hyperion Taurus");
+ */
+
+        }
+
+		private void View_Main(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+			NavigationService.RemoveBackEntry();
+		}
+    }
+}
