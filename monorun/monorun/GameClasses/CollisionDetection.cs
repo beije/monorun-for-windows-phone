016cb5f19143b0952c6071a11109734e767027e5
beije <git@benjaminhorn.se>
2013-08-03 12:07:55 +0200

    Moved out collision detection to a static class

---
+++ monorun/monorun/GameClasses/CollisionDetection.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace monorun.GameClasses
+{
+	static class CollisionDetection
+	{
+		static public bool intersectsBox(Rectangle source, Rectangle target)
+		{
+			return !(
+				((source.Y + source.Height) < (target.Y)) ||
+				(source.Y > (target.Y + target.Height)) ||
+				((source.X + source.Width) < target.X) ||
+				(source.X > (target.X + target.Width))
+			);
+
+		}
+
+		static public bool IntersectsPixel(Rectangle rect1, Color[] data1, Rectangle rect2, Color[] data2)
+		{
+			if (!intersectsBox(rect1, rect2)) return false;
+
+			int top = Math.Max(rect1.Top, rect2.Top);
+			int bottom = Math.Min(rect1.Bottom, rect2.Bottom);
+			int left = Math.Max(rect1.Left, rect2.Left);
+			int right = Math.Min(rect1.Right, rect2.Right);
+
+			for (int y = top; y < bottom; y++)
+			{
+				for (int x = left; x < right; x++)
+				{
+					Color colour1 = data1[(x - rect1.Left) + (y - rect1.Top) * rect1.Width];
+					Color colour2 = data2[(x - rect2.Left) + (y - rect2.Top) * rect2.Width];
+
+					if (colour1.A != 0 && colour2.A != 0)
+					{
+						return true;
+					}
+
+				}
+			}
+
+			return false;
+		}
+	}
+}
--- monorun/monorun/GamePage.xaml.cs
+++ monorun/monorun/GamePage.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 using monorun;
+using monorun.GameClasses;
 
 namespace monorun
 {
@@ -152,7 +153,7 @@ namespace monorun
 			if( collided ) return;
             foreach( Roland roland in rolands )
             {
-                if (IntersectsPixel(player.getItemRectangle(), player.getTextureData(), roland.getItemRectangle(), roland.getTextureData()))
+				if (CollisionDetection.IntersectsPixel(player.getItemRectangle(), player.getTextureData(), roland.getItemRectangle(), roland.getTextureData()))
                 {
                     collided = true;
 					stopAllItems();
@@ -260,43 +261,6 @@ namespace monorun
                 SpriteEffects.None,
                 0);
 
-        }
-
-        static bool intersectsBox( Rectangle source, Rectangle target ) {
-		    return !(
-                ((source.Y + source.Height) < (target.Y)) ||
-			    ( source.Y > ( target.Y + target.Height ) ) ||
-			    ( ( source.X + source.Width ) < target.X ) ||
-                (source.X > (target.X + target.Width)) 
-		    );
-
-        }
-
-        static bool IntersectsPixel(Rectangle rect1, Color[] data1, Rectangle rect2, Color[] data2)
-        {
-            if (!intersectsBox(rect1, rect2)) return false;
-
-            int top = Math.Max( rect1.Top, rect2.Top );
-            int bottom = Math.Min(rect1.Bottom, rect2.Bottom);
-            int left = Math.Max(rect1.Left, rect2.Left);
-            int right = Math.Min(rect1.Right, rect2.Right);
-
-            for (int y = top; y < bottom; y++)
-            {
-                for (int x = left; x < right; x++)
-                {
-                    Color colour1 = data1[(x - rect1.Left) + (y - rect1.Top) * rect1.Width];
-                    Color colour2 = data2[(x - rect2.Left) + (y - rect2.Top) * rect2.Width];
-
-                    if (colour1.A != 0 && colour2.A != 0)
-                    {
-                        return true;
-                    }
-                    
-                }
-            }
-
-            return false;
         }
     }   
 }
